2c6c35007c4dc13d1407494709fe2f0696b1b86e
RetroDev256 <ericpetersen3.14@gmail.com>
2022-09-02 12:09:29 -0600

    simplify alignment

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,9 @@ const linux = @import("std").os.linux;
 const snek_io = @import("snek_io.zig");
 const Snake = @import("snake.zig").Snake;
 
-pub export fn _start() callconv(.Naked) noreturn {
-    main();
-}
-
 const delay: linux.timespec = .{ .tv_sec = 0, .tv_nsec = 0x5000000 };
 
-fn main() noreturn {
-    @setAlignStack(16);
+pub export fn _start() callconv(.Naked) noreturn {
     snek_io.cbreakMode();
     var snek: Snake = undefined;
     while (true) {
