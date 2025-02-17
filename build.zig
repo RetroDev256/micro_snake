19b3a11eb53b1fa0b2f07f051558533ad35be3bc
Eric Petersen <ericpetersen3.14@gmail.com>
2025-02-16 20:19:44 -0700

    815 bytes, screw frame pointers

--- build.zig
+++ build.zig
@@ -36,6 +36,7 @@ pub fn build(b: *std.Build) !void {
         exe.link_function_sections = true;
         exe.root_module.strip = true;
         exe.root_module.single_threaded = true;
+        exe.root_module.omit_frame_pointer = true;
         exe.bundle_compiler_rt = false;
         exe.no_builtin = true;
         // further strip & stuff
